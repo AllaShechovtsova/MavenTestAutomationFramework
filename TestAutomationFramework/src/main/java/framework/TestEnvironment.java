45cd6de9f735474c98aa19cad9ea0aef52e20476
AllaShechovtsova <alusik82@gmail.com>
2017-03-13 19:08:07 +0200

    New changes are added

--- TestAutomationFramework/src/main/java/framework/TestEnvironment.java
+++ TestAutomationFramework/src/main/java/framework/TestEnvironment.java
@@ -1,24 +1,85 @@
 package framework;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class TestEnvironment {
-	public static final String DATA_FILE = "C://Users//Пользователь//git//AllaMavenTestAutomationFramework//TestAutomationFramework//src//main//java//ini//Framework.ini";
+	public static final String DATA_FILE = "C://Users//44-135//new_workspace//maven.1488027539228//TestAutomationFramework//src//main//java//ini//Framework.ini";
 	public String dataBaseConnectionString="";
-	public String  dataBaseUserName="";
+	public void setDataBaseConnectionString(String dataBaseConnectionString) {
+		this.dataBaseConnectionString = dataBaseConnectionString;
+	}
+
+	public void setDataBaseUserName(String dataBaseUserName) {
+		this.dataBaseUserName = dataBaseUserName;
+	}
+
+	public void setDataBasePassword(String dataBasePassword) {
+		this.dataBasePassword = dataBasePassword;
+	}
+
+	public void setPathToEnvironmentCreationScripts(String pathToEnvironmentCreationScripts) {
+		this.pathToEnvironmentCreationScripts = pathToEnvironmentCreationScripts;
+	}
+
+	public void setPathToTestCaseDocument(String pathToTestCaseDocument) {
+		this.pathToTestCaseDocument = pathToTestCaseDocument;
+	}
+
+	public void setPathToReportingFolder(String pathToReportingFolder) {
+		this.pathToReportingFolder = pathToReportingFolder;
+	}
+	public static String  dataBaseUserName="";
 	public String dataBasePassword="";
 	public String pathToEnvironmentCreationScripts="";
 	public String pathToTestCaseDocument="";
 	public String pathToReportingFolder="";
-private String getValueFromFile (String fileName, String attributeName){
+private String getValueFromFile (String fileName, String attributeName) throws IOException{
+	ArrayList <String> stringArray = new ArrayList<String>();
 	String value="";
+	
+	
 	//Open ini file
 	//Go throgh all rows
 	//Find row, which starts from attributeName
 	//Return the content after '=' sign 
 	
-	return value;
-}
 	
-	public void initialiseEnvironmentVariables() {
+	
+	File inputFile = new File(fileName);
+	FileInputStream fis = new FileInputStream(inputFile);
+ 
+	//Construct BufferedReader from InputStreamReader
+	BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+ 
+	String line = null;
+	
+	
+	while ((line = br.readLine()) != null) {
+		
+		if(line.length()!=0){	
+			stringArray.add(line);
+			
+		}
+	}
+	for(int i=0; i<stringArray.size();i++){
+		
+		
+			{char oneChar = ((CharSequence) stringArray).charAt(i);
+			if ((int)oneChar ==72 ){
+				
+		 value=value.substring(i+1,value.length()-1);
+	return value;
+			}}}
+	return line;}
+			
+
+	public void initialiseEnvironmentVariables() throws IOException {
 		dataBaseConnectionString=getValueFromFile(DATA_FILE, "dataBaseConnectionString" );
 		 dataBaseUserName=getValueFromFile(DATA_FILE, " dataBaseUserName" );
 		 dataBasePassword=getValueFromFile(DATA_FILE, "dataBasePassword" );
@@ -43,8 +104,10 @@ private String getValueFromFile (String fileName, String attributeName){
 
 	}
 //Debug
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
+		TestEnvironment myInstance= new TestEnvironment();
 		
+		System.out.println(myInstance.getValueFromFile(DATA_FILE,dataBaseUserName));
 	 }
 //End debug	
 }
